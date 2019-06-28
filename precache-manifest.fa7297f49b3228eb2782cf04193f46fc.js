dd26d4fec355510a438c708215df22d540beed17
levinemr2 <levinemr2@vcu.edu>
2019-06-28 15:44:39 -0400

    Updates

---
+++ precache-manifest.fa7297f49b3228eb2782cf04193f46fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e7e8b21587979b3e90ce220bb0d2a3",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "765dd8597b6d096ebe92",
+    "url": "/click-game/static/css/main.5dc94a22.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/click-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "765dd8597b6d096ebe92",
+    "url": "/click-game/static/js/main.0b2ef921.chunk.js"
+  },
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  }
+]);
