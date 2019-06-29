b278ce5497e0adff790e59d8c1e6086712d2bcda
levinemr2 <levinemr2@vcu.edu>
2019-06-29 11:23:58 -0400

    update spacing again

--- click-game/src/components/IconCard.js
+++ click-game/src/components/IconCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const IconCard = props => (
-<section id={props.id} className="tc pa3 hvr-float-shadow animated zoomInUp" value={props.id} onClick={() =>
+<section id={props.id} className="tc pa2 hvr-float-shadow animated zoomInUp" value={props.id} onClick={() =>
   props.clickedPlayer(props.id)
   }>
-    <article className="w5 pa2 ">
+    <article className="w4 pa2 ">
       <img src={props.image} className="db" alt="puppies" />
     </article>
   </section>
